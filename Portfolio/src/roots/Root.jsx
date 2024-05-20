87ac933ede0f665ed1c2c32da9da33fe2a496bec
Anna Batman <annadbatman@gmail.com>
2024-05-20 15:09:56 -0400

    Picture works now

--- Portfolio/src/roots/Root.jsx
+++ Portfolio/src/roots/Root.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import picture from "./BrazilPic.jpg"
 
 export const Root = () => {
     return (
         <div>
             <h1>Anna Batman</h1>
-            <img
-                src="./BrazilPic.jpg"
-                alt="Picture of me!"
-            />
+            <img src={picture} alt="Picture of me!" width="600" height="450" />
             <br></br>
             <Link to={"/contact"}>Contact me</Link>
             <br></br>
